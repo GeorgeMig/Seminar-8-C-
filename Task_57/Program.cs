0cb8f43f39801e77f277b08d32a3e03146668204
George_M <msgoha555@yandex.ru>
2022-06-15 23:00:37 +0300

    Ver 1.0. Task_57

---
+++ Task_57/Program.cs
@@ -0,0 +1,58 @@
+﻿int[,] arrRes = GetArray(3, 3);
+
+int[,] GetArray(int a, int b) //метод создания массива 
+{
+    int[,] array = new int[a, b];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(1, 10);
+
+        }
+    }
+    return array;
+}
+
+PrintArray(arrRes);
+
+void PrintArray(int[,] array) // метод вывода массива в терминал
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+
+        }
+        Console.WriteLine();
+    }
+}
+
+LineChangeArray(arrRes);
+
+void LineChangeArray(int[,] array) // метод переводит строки в столбцы, а столбцы в строки
+{
+    int count = 0;
+    for (int m = 0; m < array.GetLength(0); m++)
+    {
+        for (int n = 0; n < array.GetLength(1); n++)
+        {
+            if (array[m, n] == array[m, n]
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (array[m, n] == array [i,j]) count++;
+                }
+
+            }
+
+            Console.WriteLine($"{array[m,n]} встречается {count} раз");
+            count = 0;
+        }
+
+    }
+
+}
